a4197d045ba7ff39075085035b4315b2e844a707
Charly <github.womankind045@passinbox.com>
2025-07-17 18:44:21 +0330

    refactor(code): position

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,6 +2,14 @@ use std::net::TcpListener;
 
 // You can inspect what code gets generated using
 // `cargo expand --test health_check`
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+
+    tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
 
 #[actix_rt::test]
 // is the testing equivalent of the actix_web::main
@@ -25,13 +33,4 @@ async fn health_check_works() {
     // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-}
-
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
-    let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
-
-    tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
 }
