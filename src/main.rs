1e7aa2bc4f50125593449dbf7a143a31d3f12749
Charly <github.womankind045@passinbox.com>
2025-07-25 13:59:11 +0330

    feat(app): add central configuration

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use sqlx::postgres::PgPool;
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
-mod routes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("Failed to read configurationg.");
     let listener = TcpListener::bind("127.0.0.1:8000")?;
     println!("Listening to {}", listener.local_addr().unwrap().port());
     run(listener)?.await?;
