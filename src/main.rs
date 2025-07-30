71970855799d225e3483af7e2827401837fc1292
Charly <github.womankind045@passinbox.com>
2025-07-31 00:11:02 +0330

    feat(log): add context

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // fall back to printing all spans at info-level or above
+    // if RUST_LOG env variable as not been set.
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber).expect("Failed to set subscriber");
+
     let configuration = get_configuration().expect("Failed to read configurationg.");
     let connection = PgPool::connect(&configuration.database.connection_string())
         .await
