26ec0c28dffdfba7c99428fb7ea2dac326b7f2e4
Charly <github.womankind045@passinbox.com>
2025-08-11 13:59:21 +0330

    refactor(telemetry): optional log for tests

--- tests/common.rs
+++ tests/common.rs
@@ -1,8 +1,20 @@
+use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
 use zero2prod::configuration::{DatabaseSettings, get_configuration};
 use zero2prod::startup::run;
+use zero2prod::telemetry;
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = telemetry::get_subscriber("test".into(), "debug".into(), std::io::stdout);
+        telemetry::init_subscriber(subscriber);
+    } else {
+        let subscriber = telemetry::get_subscriber("test".into(), "debug".into(), std::io::sink);
+        telemetry::init_subscriber(subscriber);
+    }
+});
 
 #[allow(dead_code)]
 pub struct TestApp {
@@ -13,6 +25,10 @@ pub struct TestApp {
 // You can inspect what code gets generated using
 // `cargo expand --test health_check`
 pub async fn spawn_app() -> TestApp {
+    // The first time `initialize` is invoked the code in `TRACING` is executed.
+    // All other invocations will instead skip execution.
+    Lazy::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
